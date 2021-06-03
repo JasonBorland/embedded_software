2e16b28b2b556a2b2e9426e2d543e320f124d42c
jason <jason.borland@gmail.com>
2021-06-03 21:10:08 +0100

    first lesson

---
+++ lesson1/main.c
@@ -0,0 +1,25 @@
+
+int main() {
+   unsigned int counter;
+      counter = 0;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+
+    return 0;
+}
